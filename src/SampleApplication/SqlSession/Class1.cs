a83ac10c9b8de973f91e8050ba3538ad93d900e7
dileep <dileepkumarponnam@gmail.com>
2019-06-27 22:11:56 +0530

    Sourc code added

---
+++ src/SampleApplication/SqlSession/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SqlSession
+{
+    public class Class1
+    {
+        public static void Main()
+        {
+            int a = 10;
+            int b = 20;
+            int c = a + b;
+            Console.WriteLine("Sum is ..." + "C");
+        }
+    }
+}
