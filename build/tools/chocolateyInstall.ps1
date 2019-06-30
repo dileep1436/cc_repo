5488c2b50fb65cb2c715c45f756f65b9dbbe92d0
dileep <dileepkumarponnam@gmail.com>
2019-06-30 11:56:08 +0530

    Choco scripts added

--- build/tools/chocolateyInstall.ps1
+++
@@ -1,6 +0,0 @@
-# Custom value: [[CustomValue]]
-$ErrorActionPreference = 'Stop'; # stop on all errors
-
-[[AutomaticPackageNotesInstaller]]
-$packageName  = 'project'
-$toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
